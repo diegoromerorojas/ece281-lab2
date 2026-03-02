78ec1d2188f0ae2d7a1cc7b586bafaf8794d6205
diegoromerorojas <225315540+diegoromerorojas@users.noreply.github.com>
2026-03-01 17:02:47 -0700

    Lab 2 complete, TB and sevenseg_decoder arch fixed: inverted all bits. Top design complete and runs successfully

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -31,9 +31,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity sevenseg_decoder is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+entity sevenseg_decoder is Port( 
+           i_Hex : in STD_LOGIC_VECTOR (3 downto 0):= (others=> '0');
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0):= (others=> '0'));
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
@@ -41,21 +41,22 @@ architecture Behavioral of sevenseg_decoder is
 begin
 -- concurrent statements
 with i_Hex select
-o_seg_n <=  "0111111" when "0000",
-            "0000110" when "0001",
-            "1011011" when "0010",
-            "1001111" when "0011",
-            "1100110" when "0100",
-            "1101101" when "0101",
-            "1111101" when "0110",
-            "0000111" when "0111",
-            "1111111" when "1000",
-            "1100111" when "1001",
-            "1110111" when "1010",
-            "1111100" when "1011",
-            "1011000" when "1100",
-            "1011110" when "1101",
-            "1111001" when "1110",
-            "1110001" when "1111";
+o_seg_n <=  "1000000" when "0000", -- MSB and LSB are inverse of what is on the derived truth table from preLab 2
+            "1111001" when "0001",
+            "0100100" when "0010",
+            "0110000" when "0011",
+            "0011001" when "0100",
+            "0010010" when "0101",
+            "0000010" when "0110",
+            "1111000" when "0111",
+            "0000000" when "1000",
+            "0011000" when "1001",
+            "0001000" when "1010",
+            "0000011" when "1011",
+            "0100111" when "1100",
+            "0100001" when "1101",
+            "0000110" when "1110",
+            "0001110" when "1111",
+            "1111111" when others;
                          
 end Behavioral;
--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -32,12 +32,78 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder_tb is
---  Port ( );
+--  Port ( ); no ports needed in TB
 end sevenseg_decoder_tb;
 
 architecture Behavioral of sevenseg_decoder_tb is
+    component sevenseg_decoder is port
+    (
+    i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+    o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+    
+    end component;
+    
+--declare internal wires/signals
+signal w_sw : std_logic_vector (3 downto 0):= (others=> '0');
+signal w_seg : std_logic_vector (6 downto 0):= (others=> '0');
+
 
 begin
+--UUT declaration
+sevenseg_decoder_inst : sevenseg_decoder port map(
+    i_Hex(0) => w_sw(0),
+    i_Hex(1) => w_sw(1),
+    i_Hex(2) => w_sw(2),
+    i_Hex(3) => w_sw(3),
+    
+    o_seg_n(0) => w_seg(0),
+    o_seg_n(1) => w_seg(1),
+    o_seg_n(2) => w_seg(2),
+    o_seg_n(3) => w_seg(3),
+    o_seg_n(4) => w_seg(4),
+    o_seg_n(5) => w_seg(5),
+    o_seg_n(6) => w_seg(6)
+);
 
-
+    -- PROCESSES ----------------------------------------	
+	-- Test Plan Process --------------------------------
+	-- Implement the test plan here.  Body of process is continuous from time = 0  
+	test_process : process 
+	begin
+	-- Place test cases here.
+		w_sw <= x"0"; wait for 10 ns;
+            assert w_seg = "1000000" report "error on x0" severity failure;
+        w_sw <= x"1"; wait for 10 ns;
+            assert w_seg = "1111001" report "error on 1" severity failure;
+        w_sw <= x"2"; wait for 10 ns;
+            assert w_seg = "0100100" report "error on 2" severity failure;
+        w_sw <= x"3"; wait for 10 ns;
+            assert w_seg = "0110000" report "error on 3" severity failure;
+        w_sw <= x"4"; wait for 10 ns;
+            assert w_seg = "0011001" report "error on 4" severity failure;
+        w_sw <= x"5"; wait for 10 ns;
+            assert w_seg = "0010010" report "error on 5" severity failure;
+        w_sw <= x"6"; wait for 10 ns;
+            assert w_seg = "0000010" report "error on 6" severity failure;
+        w_sw <= x"7"; wait for 10 ns;
+            assert w_seg = "1111000" report "error on 7" severity failure;
+        w_sw <= x"8"; wait for 10 ns;
+            assert w_seg = "0000000" report "error on 8" severity failure;
+        w_sw <= x"9"; wait for 10 ns;
+            assert w_seg = "0011000" report "error on 9" severity failure;
+        w_sw <= x"A"; wait for 10 ns;
+            assert w_seg = "0001000" report "error on xA" severity failure;
+        w_sw <= x"B"; wait for 10 ns;
+            assert w_seg = "0000011" report "error on xB" severity failure;
+        w_sw <= x"C"; wait for 10 ns;
+            assert w_seg = "0100111" report "error on xC" severity failure;
+        w_sw <= x"D"; wait for 10 ns;
+            assert w_seg = "0100001" report "error on xD" severity failure;
+        w_sw <= x"E"; wait for 10 ns;
+            assert w_seg = "0000110" report "error on xE" severity failure;
+        w_sw <= x"F"; wait for 10 ns;
+            assert w_seg = "0001110" report "error on xF" severity failure;
+		wait; -- wait forever
+	end process;	
+	-----------------------------------------------------	
 end Behavioral;
